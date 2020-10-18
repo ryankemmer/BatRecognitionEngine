09ad80df1497710eb3b0dec995372624502925dc
Ryan Kemmer <ryankemmer@Ryans-MacBook-Pro-8.local>
2020-10-18 13:42:59 -0700

    upload video functionality

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
+const multer = require("multer");
 
 var app = express();
+
+const storage = multer.diskStorage({
+  destination: './public/videos',
+  filename: function (req, file, cb) {
+      console.log('file uploaded', file);
+      cb(null, `test.mp4`);
+  }
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
 app.use('/', indexRouter);
@@ -37,5 +46,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const multer = require("multer");
+
+
+const storage = multer.diskStorage({
+  destination: './public/videos',
+  filename: function (req, file, cb) {
+      console.log('file uploaded', file);
+      cb(null, `test.mp4`);
+  }
+})
+
+var upload = multer({storage: storage});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.post('/upload', function (request, response) {
+  upload(request, response, function(err) {
+      if(err) {
+          console.log('Error Occured'); 
+          return; 
+      }
+      console.log(request.file);
+      response.end('Your file Uploaded'); 
+      console.log('Video Uploaded'); 
+  })
+}); 
 
 module.exports = router;
